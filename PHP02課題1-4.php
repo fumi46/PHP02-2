ea44afc81f3c906866926687d151fa9a311bb9dc
fumi46 <71321992+fumi46@users.noreply.github.com>
2020-09-24 19:07:27 +0900

    php02課題

---
+++ PHP02課題1-4.php
@@ -0,0 +1,19 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a+$b;
+
+
+$array = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array[7];
+
+
+$hello = "Hello,";
+$name = "fumi";
+$world = "'s World!";
+echo $hello.$name.$world;
+
+
+$tech_boost = "「tech";
+$tech_boost .= " boost」";
+echo $tech_boost;
